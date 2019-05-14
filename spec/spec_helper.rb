44ea22584d85f34827cf47f04a7016cbdc11b10a
Sam Markham <sam.e.markham@gmail.com>
2019-05-14 18:25:38 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative '../lib/operations.rb'
 
-RSpec.configure do |config|
+#RSpec.configure do |config|
+
   # config here
+#end
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'default'
 end
